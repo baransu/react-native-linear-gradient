4d1c29db11929fa3f98759e1758e54aee6e119ac
Sunbreak <sunbreak.wang@gmail.com>
2023-03-03 18:58:56 +0800

    Support Fabric on iOS

--- schema/LinearGradientNativeComponent.js
+++ schema/LinearGradientNativeComponent.js
@@ -4,13 +4,14 @@ import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropT
 import type { HostComponent } from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
 import type { PointValue } from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 import type { Float } from 'react-native/Libraries/Types/CodegenTypes';
+import type { ColorValue } from "react-native/Libraries/StyleSheet/StyleSheet";
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
 export type Props = $ReadOnly<{|
   ...ViewProps,
   startPoint?: PointValue,
   endPoint?: PointValue,
-  colors: $ReadOnlyArray<Float>,
+  colors: $ReadOnlyArray<ColorValue>,
   locations?: $ReadOnlyArray<Float>,
   useAngle?: boolean,
   angleCenter?: PointValue,
